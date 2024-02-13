6b02a94bf3109e01d5ed9bebfaea0914e072d64f
Farouk Bello <faroukk.bello@gmail.com>
2024-02-13 15:30:20 +0100

    added deployment rules

--- Components/ContactComponent.tsx
+++ Components/ContactComponent.tsx
@@ -18,7 +18,7 @@ const ContactComponent = () => {
                             <input className='m-4' placeholder='Alternative Email*' style={{ borderRadius: '.3rem', border: '1px solid #998E8E', width: '316px', margin: '.5rem', padding: '.5rem' }} />
                             <input className='m-4' placeholder='+234   |' style={{ borderRadius: '.3rem', border: '1px solid #998E8E', width: '316px', margin: '.5rem', padding: '.5rem' }} />
                             <div className='relative'>
-                                <section className='flex' style={{ position: 'absolute', bottom: '25px', right: '23px' , width: '67px'}}>
+                                <section className='flex' style={{ position: 'absolute', bottom: '25px', right: '23px', width: '67px' }}>
                                     <Image alt='' className='h-fit' style={{ width: '17%' }} src={attach} />
                                     <p style={{ color: '#3333338F', fontSize: 'x-small' }}>
                                         Attach files
@@ -26,14 +26,14 @@ const ContactComponent = () => {
                                 </section>
                                 <textarea className='m-4' placeholder='Tell us about your project*' style={{ borderRadius: '.3rem', border: '1px solid #998E8E', width: '316px', height: '120px', margin: '.5rem', padding: '.5rem' }} />
                             </div>
-                            <div className='p-4'style={{paddingTop: '0'}}>
+                            <div className='p-4' style={{ paddingTop: '0' }}>
                                 <section className='flex'>
-                                    <input type="checkbox" style={{width:'19px !important'}} />
-                                    <label style={{ fontSize: 'small',marginLeft: '.5rem', color: '#3333338F' }}>
+                                    <input type="checkbox" style={{ width: '19px !important' }} />
+                                    <label style={{ fontSize: 'small', marginLeft: '.5rem', color: '#3333338F' }}>
                                         Request NDA
                                     </label>
                                 </section>
-                                <p style={{ fontSize: 'x-small',paddingTop: '1rem',color: '#3333338F'}}>
+                                <p style={{ fontSize: 'x-small', paddingTop: '1rem', color: '#3333338F' }}>
                                     By sending this form I confirm that I have read and accept Int+
                                     <a style={{ color: '#2D7EFF', marginLeft: '.5rem' }} href="https://www.sportybet.com/ng/m/">Privacy Policy</a>
                                 </p>
@@ -45,7 +45,7 @@ const ContactComponent = () => {
                     </section>
                     <section className="contact-text" style={{ color: 'white', width: '44%', display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
                         <h1 style={{ fontSize: '20px', fontWeight: '500', marginBottom: '1rem' }}>
-                            WHAT'S NEXT?
+                            WHAT&apos;S NEXT?
                         </h1>
                         <ul className='contact-list'>
                             <li style={{ listStyle: 'outside' }}>
